7a00a6da26641c73b2827d4891f29e41d1dd6787
aaqilap <amp10098@nyu.edu>
2026-05-05 14:27:52 -0400

    task status - state of task updates

--- src/TaskStatus.java
+++ src/TaskStatus.java
@@ -1,6 +1,27 @@
+/**
+ * what state a task is in.
+ *
+ * we use an enum so we get a fixed, type-safe list of states
+ * (todo, in_progress, complete) instead of random strings.
+ */
 public enum TaskStatus {
-    TODO, 
-    IN_PROGRESS, 
-    COMPLETE
-    
+    TODO,
+    IN_PROGRESS,
+    COMPLETE;
+
+    /**
+     * forgiving parser for when the user types a status into the cli.
+     * if we can't figure it out, we just default to todo.
+     */
+    public static TaskStatus fromString(String value) {
+        if (value == null) {
+            return TODO;
+        }
+        String normalized = value.trim().toUpperCase().replace(' ', '_');
+        try {
+            return TaskStatus.valueOf(normalized);
+        } catch (IllegalArgumentException ex) {
+            return TODO;
+        }
+    }
 }
